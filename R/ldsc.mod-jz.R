f0b54d259aec4a42389ea1c84ea4319b7843f9d1
Johan Källberg Zvrskovec <johan.akz@gmail.com>
2024-02-26 16:12:38 +0000

    package support for ldsc.mod

--- R/ldsc.mod-jz.R
+++ R/ldsc.mod-jz.R
@@ -234,7 +234,7 @@ ldsc.mod <- function(traits, sample.prev, population.prev, ld, wld,
   }
 
   if(!(is.null(trait.names))){
-    check_names<-str_detect(trait.names, "-")
+    check_names<-stringr::str_detect(trait.names, "-")
     if(any(check_names))
       warning("Your trait names specified include mathematical arguments (e.g., + or -) that will be misread by lavaan. Please rename the traits using the trait.names argument.")
   }
