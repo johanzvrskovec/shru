8d0cf7fe0e6d5933c76727303d55cf7c02238fdc
Johan Källberg Zvrskovec <johan.akz@gmail.com>
2025-11-21 09:36:27 +0000

    startup message

---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+#Package information on load
+
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage("\nSHared R Utilities (SHRU) for behavioural genomics and beyond.\nIf you use this resource, please consider citing: https://doi.org/10.1101/2025.04.07.25324946\n")
+}
